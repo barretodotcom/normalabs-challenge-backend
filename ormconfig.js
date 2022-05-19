9d253c6813dd53cfa19afed3050975b049044f78
Lucas Barreto <lucas_barreto29@outlook.com>
2022-05-19 15:29:31 -0300

    added tags in ormconfig

--- ormconfig.js
+++ ormconfig.js
@@ -1,11 +1,11 @@
-console.log(process.env.DATABASE_URL);
-
-module.exports = {
-    type: 'postgres',
-    url: process.env.DATABASE_URL,
-    migrations: ['./dist/shared/typeorm/migrations/*.js'],
-    cli: {
-        migrationsDir: ['./dist/shared/typeorm/migrations'],
+const ORMConfig = {
+    "type": 'postgres',
+    "url": process.env.DATABASE_URL,
+    "migrations": ['./dist/shared/typeorm/migrations/*.js'],
+    "cli": {
+        "migrationsDir": ['./dist/shared/typeorm/migrations/'],
     },
-    entities: ['./dist/modules/**/typeorm/entities/**.js'],
+    "entities": ['./dist/modules/**/typeorm/entities/*.js'],
+    "ssl": true, "extra": { "ssl": { "rejectUnauthorized": false }}
 };
+module.exports = ORMConfig
